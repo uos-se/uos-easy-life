6da6f65172e0f0f4566d289206196883326a98ae
unknownpgr <unknownpgr@gmail.com>
2024-10-13 08:49:00 +0000

    Add mock course DB

--- backend/src/main/java/kr/ac/uos/uos_easy_life/infra/MockCourseRepository.java
+++ backend/src/main/java/kr/ac/uos/uos_easy_life/infra/MockCourseRepository.java
@@ -1,9 +1,15 @@
 package kr.ac.uos.uos_easy_life.infra;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Repository;
+
 import kr.ac.uos.uos_easy_life.core.interfaces.CourseRepository;
 import kr.ac.uos.uos_easy_life.core.model.Course;
 
@@ -12,6 +18,27 @@ public class MockCourseRepository implements CourseRepository {
 
   // Course 리스트를 이용해 데이터를 저장
   private List<Course> courses = new ArrayList<>();
+
+  public MockCourseRepository() throws IOException {
+    InputStream inputStream = getClass().getClassLoader().getResourceAsStream("mock-course-data.json");
+    // JSON 파일을 읽어와 Course 객체로 변환 후 저장
+    JSONObject json = new JSONObject(new String(inputStream.readAllBytes(), "UTF-8"));
+    JSONArray courseList = json.getJSONArray("dsMain");
+    for (int i = 0; i < courseList.length(); i++) {
+      JSONObject courseJson = courseList.getJSONObject(i);
+
+      String id = "COURSE_" + (i + 1);
+      String lectureName = courseJson.getString("SBJC_NM");
+      String lectureCode = courseJson.getString("SBJC_NO");
+      int lectureCredit = courseJson.getInt("CMPN_PNT");
+      int lectureGrade = 1; // FIXME: 학년 정보를 3/4따위로 문자열로 박아놓은 경우가 있어서 임시로 1로 설정
+
+      Course course = new Course(id, lectureName, lectureCode, lectureCredit,
+          lectureGrade);
+
+      courses.add(course);
+    }
+  }
 
   @Override
   public Course findById(String id) {
