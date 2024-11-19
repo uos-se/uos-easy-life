bcef55a23fc5829ca8f143fe28addd45bc61c228
HyunJemmI <dlguswp010731@gmail.com>
2024-11-19 10:13:11 +0000

    Add Engineering Credit for academic status

--- backend/src/main/java/kr/ac/uos/uos_easy_life/core/services/UserService.java
+++ backend/src/main/java/kr/ac/uos/uos_easy_life/core/services/UserService.java
@@ -110,6 +110,8 @@ public class UserService {
         majorCompletedCredit,
         liberalRequiredCredit,
         liberalCompletedCredit,
+        engineeringRequiredCredit,
+        engineeringCompletedCredit,
         generalRequiredCredit,
         generalCompletedCredit,
         minimumTotalGradePointAverage,
