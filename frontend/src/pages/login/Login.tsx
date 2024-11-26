7b5875b6f610de9e8ff0e3fa8daefad60c1efad0
beming-dev <mingfordev@gmail.com>
2024-11-26 02:25:18 +0000

    Fix login ui disappear error

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -14,7 +14,7 @@ export function Login() {
 
   return (
     <>
-      <div className="relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative grid h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-indigo-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
@@ -30,7 +30,7 @@ export function Login() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">로그인</h1>
