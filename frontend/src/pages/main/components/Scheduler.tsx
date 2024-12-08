9d2e4bc9dace7a4fb1e2fb8da2c8826ea42318ab
hyeok <cod3holic@gmail.com>
2024-12-08 15:05:09 +0000

    Add first loading ui

--- frontend/src/pages/main/components/Scheduler.tsx
+++ frontend/src/pages/main/components/Scheduler.tsx
@@ -80,7 +80,7 @@ export function Scheduler() {
               마지막 학기 지우기
             </button>
           </div>
-          <div className="flex flex-row h-full w-full overflow-x-scroll p-x-4 py-0">
+          <div className="flex flex-row h-full w-full overflow-x-scroll px-4 py-0">
             {semesters !== undefined && (
               <>
                 {semesters.map((semester) => (
@@ -97,9 +97,9 @@ export function Scheduler() {
               onClick={() =>
                 addSemesters(userInfo!.grade! * 2 + semesters!.length)
               }
-              className="bg-muted min-w-[300px] max-w-[300px] text-white px-4 py-2 rounded hover:bg-muted-foreground transition border-dashed border-2"
+              className="bg-muted min-w-[300px] max-w-[300px] text-gray-500 px-4 py-2 rounded hover:bg-muted-foreground transition border-dashed border-2"
             >
-              +
+              추가하기 +
             </button>
           </div>
         </div>
